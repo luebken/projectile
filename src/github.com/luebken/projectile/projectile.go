9c7214d940d6b2e5907d6d0117c2e83da958f71a
Matthias Luebken <matthias.luebken@gmail.com>
2013-09-16 22:59:39 +0200

    board id and csvoutput option as command line arg

--- src/github.com/luebken/projectile/projectile.go
+++ src/github.com/luebken/projectile/projectile.go
@@ -6,12 +6,44 @@ import (
 	"github.com/luebken/projectile/trello"
 	"log"
 	"os"
+	"regexp"
 )
 
-//TODO trello board as commandline parameter
-func main() {
+type Args struct {
+	Board     string
+	OutputCSV bool
+}
 
-	body := trello.CallTrello("board/522730ae9504e7ed3d0038e2/cards")
+func extractCommandLineArgs(args []string) Args {
+	boardid := "<undefined>"
+	outputcsv := false
+
+	for i := 0; i < len(args); i++ {
+		//board-id
+		re := regexp.MustCompile("boardid:(.*)")
+		matches := re.FindStringSubmatch(args[i])
+		if len(matches) > 0 {
+			boardid = matches[1]
+		}
+		//csvoutput
+		re = regexp.MustCompile("csvoutput:(.*)")
+		matches = re.FindStringSubmatch(args[i])
+		if len(matches) > 0 {
+			outputcsv = (matches[1] == "true")
+		}
+	}
+
+	if boardid == "<undefined>" {
+		log.Fatal("Usage: projectile boardid:<boardid> csvoutput:false")
+	}
+
+	return Args{boardid, outputcsv}
+}
+
+func main() {
+	args := extractCommandLineArgs(os.Args)
+
+	body := trello.CallTrello("board/" + args.Board + "/cards")
 
 	// JSON parsing
 	var cards []trello.Card
@@ -19,13 +51,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
-	//fmt.Printf("%+v", cards)
 
 	// Output
 	writer := csv.NewWriter(os.Stdout)
 
-	//		  fo, err := os.Create("output.csv")
-	//		writer := csv.NewWriter(fo)
+	if args.OutputCSV {
+		log.Println("writing to output.csv")
+		fo, err := os.Create("output.csv")
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+		writer = csv.NewWriter(fo)
+	}
 
 	writer.Write([]string{"Arbeitsbereich", "Arbeitspaket", "Status", "Startdatum", "Enddatum", "Kollegen"})
 	for _, card := range cards {
